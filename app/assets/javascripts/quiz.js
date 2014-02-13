e8daaf4121069a83c2c6a6a122670d7556e7310c
Nikki Thean <nikki.thean@gmail.com>
2014-02-13 12:28:40 -0500

    Add flipping card transitions

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,71 +1,72 @@
 var width = $("#map").width(),
-  height = $("#map").height(),
-  projection = d3.geo.orthographic()
-                       .scale(height/2.3)
-                       .translate([width/2, height/2])
+    height = $("#map").height(),
+    projection = d3.geo.orthographic()
+                       .scale(height / 2.3)
+                       .translate([width / 2, height / 2])
                        .clipAngle(90),
-  path = d3.geo.path().projection(projection),
-  svg = d3.select("#map").append("svg").attr("width", width).attr("height", height),
-  $inputBox = $("#answer"),
-  $countryBox = $("#answer_country_id"),
-  $skipButton = $("#skip"),
-  names = {},
-  clickable = true,
-  answer, countries, country;
+    path = d3.geo.path().projection(projection),
+    svg = d3.select("#map").append("svg").attr("width", width).attr("height", height),
+    $inputBox = $("#answer"),
+    $countryBox = $("#answer_country_id"),
+    $nextButton = $("#next"),
+    $notification = $("#notification"),
+    names = {},
+    clickable = true,
+    answer,
+    countries,
+    country;
 
 svg.append("path")
-   .datum({type: "Sphere"})
-   .attr("id", "globe")
-   .attr("d", path);
+    .datum({type: "Sphere"})
+    .attr("id", "globe")
+    .attr("d", path);
 
 queue()
-.defer(d3.json, "/datafiles/world.json")
-.defer(d3.tsv, "/datafiles/countrynames.tsv")
-.await(ready);
+    .defer(d3.json, "/datafiles/world.json")
+    .defer(d3.tsv, "/datafiles/countrynames.tsv")
+    .await(ready);
 
 $(document).ready(function() {
   $inputBox.keypress(function(event) {
     if (clickable === true && event.which === 13) {
       var $this = $(this),
           value = $this.val(),
-          active = d3.select(".active").classed("active", false);;
+          active = d3.select(".active").classed("active", false);
 
       if (checkAnswer(value)) {
         active.classed("correct", true);
-        removeCountry();
+        setNotification("correct");
       } else {
         active.classed("wrong", true);
-        removeCountry();
+        setNotification("wrong");
       }
 
+      removeCountry();
       toggleInput();
     }
   });
 
-  $skipButton.on("click", function(e) {
-    if (clickable === true) {
-      changeCountry();
-      clearBoxes();
-      transition();
+  $nextButton.on("click", function(e) {
+    if (!clickable) {
+      toggleInput();
     }
+    $(".info-container").slideUp();
+    changeCountry();
+    clearBoxes();
+    transition();
+  });
+
+  $("a.flip").on("click", function(e) {
+    e.preventDefault();
+    $(".card").toggleClass("flipped");
   });
 
   $("#new_answer").on("ajax:success", function(e, data, status, xhr) {
     $.get("/countries/" + country.id, function(data) {
-      $(".info-container").html(data);
-      $(".info-container").slideDown();
-      $("#next").slideDown();
+      $("#countrydata").html(data);
     })
-    changeCountry();
-    clearBoxes();
+    $(".info-container").slideDown();
   });
-
-  $("#next").on("click", function(e) {
-    $("#next").slideUp();
-    $(".info-container").slideUp();
-    transition();
-    toggleInput();
-  })
 })
 
 function ready(error, world, places) {
@@ -137,6 +138,14 @@ function checkAnswer(input) {
     return (input.toLowerCase() === shortAnswer.toLowerCase());
   } else {
     return (input.toLowerCase() === answer.toLowerCase());
+  }
+}
+
+function setNotification(command) {
+  if (command === "correct") {
+    $notification.html("<h1>Good job!</h1>");  
+  } else if (command === "wrong") {
+    $notification.html("<h1>Nope! It's " + answer + ".</h1>")
   }
 }
 
