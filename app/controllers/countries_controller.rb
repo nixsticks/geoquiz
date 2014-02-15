ac82dd87e9bf33b039f83ada07e54851ca20f65e
Nikki Thean <nikki.thean@gmail.com>
2014-02-14 21:14:53 -0500

    Sort answers properly this time

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,7 +2,7 @@ class CountriesController < ApplicationController
   def show
     @country = Country.find(params[:id])
     @answers = @country.get_percentages
-    gon.data = @answers.sort_by {|answer| answer[:percentage] }
+    gon.data = @answers.sort_by {|answer| answer[:percentage] }.reverse
     render layout: false
   end
 end
