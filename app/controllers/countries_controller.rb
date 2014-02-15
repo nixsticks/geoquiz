a129f81d470f83cefcdd4124b6193ce03046e4d6
Nikki Thean <nikki.thean@gmail.com>
2014-02-14 21:09:13 -0500

    Update country name for China, sort answers properly

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,7 +2,7 @@ class CountriesController < ApplicationController
   def show
     @country = Country.find(params[:id])
     @answers = @country.get_percentages
-    gon.data = @answers
+    gon.data = @answers.sort_by {|answer| answer.percentage}
     render layout: false
   end
 end
