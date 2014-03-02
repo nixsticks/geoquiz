a881442555f197216b6b75d723a413442338a5b7
Nikki Thean <nikki.thean@gmail.com>
2014-03-02 08:11:18 -0500

    Fix proportions

--- app/assets/javascripts/barchart.js
+++ app/assets/javascripts/barchart.js
@@ -7,8 +7,8 @@ var data = gon.data,
               .range([0, width]),
     chart = d3.select("#unitdata").append("svg")
             .attr("class", "barchart")
-            .attr("viewBox", "0 0 " + width + " " + height)
-            .attr("preserveAspectRatio", "xMidYMid");
+            .attr("width", width)
+            .attr("height", height);
     bar = chart.selectAll("g")
              .data(data)
              .enter().append("g")
